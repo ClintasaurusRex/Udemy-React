3fdc930e7046a7e3bc59796b0e581d6b826b7118
Clint <clinthiles1992@gmail.com>
2025-01-06 13:18:59 -0800

    Update App component to display fetched advice instead of static text

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ export default function App() {
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
+    // console.log(data.slip.advice);
     setAdvice(data.slip.advice);
   }
 
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
     </div>
   );

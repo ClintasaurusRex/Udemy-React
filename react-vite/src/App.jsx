a966158dd435fbe45dc2fed5cbb1b26add91120e
Clint <clinthiles1992@gmail.com>
2025-01-09 08:35:36 -0800

    Initialize pizza-menu project with React and Vite setup

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 export default function App() {
   const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
+  const [countBtn, setCountBtn] = useState(0);
 
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
@@ -15,11 +16,27 @@ export default function App() {
     getAdvice();
   }, []);
 
+  const handleCount = function (btn) {
+    if (btn === "increase") {
+      setCountBtn(countBtn + 1);
+    } else if (btn === "decrease") {
+      setCountBtn(countBtn - 1);
+    } else if (btn === "multiply") {
+      setCountBtn(countBtn * 2);
+    }
+  };
+
   return (
     <div>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
       <Message count={count} />
+      <div className="countBtns">
+        <button onClick={() => handleCount("increase")}>Increase Count</button>
+        <button onClick={() => handleCount("decrease")}>Decrease Count</button>
+        <button onClick={() => handleCount("multiply")}>multiply Count</button>
+        <p>{countBtn}</p>
+      </div>
     </div>
   );
 }

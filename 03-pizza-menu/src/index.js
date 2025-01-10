6513ced5e4eb0dc98aeffee5287b31973ea776f3
Clint <clinthiles1992@gmail.com>
2025-01-10 10:35:35 -0800

    Refactor pizza menu application to include Header, Menu, and Footer components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -49,15 +49,50 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  return <h1 style={style}>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
       <Pizza />
     </div>
   );
 }
 
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("Were currently open");
+  // else alert("Sorry were closed");
+
+  const time = new Date().toLocaleTimeString();
+  return <footer>{time}. We're currently open</footer>;
+
+  // return React.createElement("footer", null, "We're currently open");
+}
+
 function Pizza() {
   return (
     <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
       <h2>Pizza Spinaci</h2>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
